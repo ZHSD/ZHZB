e531ffc1a1df20cacaee6760a3aaf07ac1528907
张浩 <zhanghao@localhost>
2016-11-15 12:00:53 +0800

    项目框架搭建

---
+++ ZHZB/ZHZB/Classes/Main/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  ZHZB
+//
+//  Created by 张浩 on 2016/11/15.
+//  Copyright © 2016年 张浩. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        addChildVC(storyName: "Home")
+        addChildVC(storyName: "Live")
+        addChildVC(storyName: "Follow")
+        addChildVC(storyName: "Profile")
+    }
+
+    private func addChildVC(storyName:String) {
+        let Vc = UIStoryboard(name: storyName, bundle: nil).instantiateInitialViewController()!
+        addChildViewController(Vc)
+    }
+    
+
+
+}
